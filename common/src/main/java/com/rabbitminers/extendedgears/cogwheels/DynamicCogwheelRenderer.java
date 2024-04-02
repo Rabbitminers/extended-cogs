fa662fd9e90b258eede25e5b822f3c7324e56e5b
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-04-02 01:16:05 +0100

    Corrected Edge case when using bamboo for cogwheels

--- common/src/main/java/com/rabbitminers/extendedgears/cogwheels/DynamicCogwheelRenderer.java
+++ common/src/main/java/com/rabbitminers/extendedgears/cogwheels/DynamicCogwheelRenderer.java
@@ -17,21 +17,15 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-
-import java.util.Random;
 
 public class DynamicCogwheelRenderer {
     public static final Compartment<CogwheelModelKey> COGWHEEL = new Compartment<>();
@@ -39,7 +33,7 @@ public class DynamicCogwheelRenderer {
     public static final StitchedSprite STRIPPED_LOG_TEMPLATE = new StitchedSprite(new ResourceLocation("block/stripped_spruce_log"));
     public static final StitchedSprite STRIPPED_LOG_TOP_TEMPLATE = new StitchedSprite(new ResourceLocation("block/stripped_spruce_log_top"));
 
-    public static final String[] LOG_SUFFIXES = new String[] { "_log", "_stem" };
+    public static final String[] LOG_SUFFIXES = new String[]{"_log", "_stem", "_block"};
 
     public static @NotNull BakedModel generateModel(CogwheelModelKey key) {
         ClientCogwheelMaterial material = CogwheelMaterialManager.clientOf(key.material());
@@ -76,21 +70,24 @@ public class DynamicCogwheelRenderer {
     public static BlockState getStrippedLogState(String namespace, String wood) {
         for (String suffix : LOG_SUFFIXES) {
             Optional<BlockState> state =
-                    Registry.BLOCK.getHolder(ResourceKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(namespace,  "stripped_" + wood + suffix)))
+                    Registry.BLOCK.getHolder(ResourceKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(namespace, "stripped_" + wood + suffix)))
                             .map(Holder::value)
                             .map(Block::defaultBlockState);
             if (state.isPresent())
                 return state.get();
         }
-        return Blocks.OAK_LOG.defaultBlockState();
+        return Blocks.STRIPPED_OAK_LOG.defaultBlockState();
     }
 
     public static TextureAtlasSprite getSpriteOnSide(BlockState state, Direction side) {
         BakedModel model = Minecraft.getInstance()
                 .getBlockRenderer()
                 .getBlockModel(state);
-        if (model == null)
+
+        // Should never happen but seems to occur at runtime?
+        if (model == null) {
             return null;
+        }
 
         RandomSource random = RandomSource.create();
         random.setSeed(42L);
